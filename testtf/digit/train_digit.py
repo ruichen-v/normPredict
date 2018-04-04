53c7ec6177cff2eccf1500ce3c211f8247d2a23f
richen <richen@umich.edu>
2018-04-04 17:28:20 -0400

    digit done with self-written dataset

--- testtf/digit/train_digit.py
+++ testtf/digit/train_digit.py
@@ -9,15 +9,15 @@ Project: https://github.com/aymericdamien/TensorFlow-Examples/
 from __future__ import division, print_function, absolute_import
 
 import tensorflow as tf
+import numpy as np
+import gzip
 
-# Import MNIST data
-from tensorflow.examples.tutorials.mnist import input_data
-mnist = input_data.read_data_sets("/tmp/data/", one_hot=True)
+IMAGE_SIZE = 28
+NUM_CHANNELS = 1
 
 # Training Parameters
-learning_rate = 0.001
-num_steps = 200
-batch_size = 128
+learning_rate = 0.0001
+num_epochs = 2
 display_step = 10
 
 # Network Parameters
@@ -27,9 +27,38 @@ num_classes = 10 # MNIST total classes (0-9 digits)
 # tf Graph input
 X = tf.placeholder(tf.float32, [None, num_input])
 Y = tf.placeholder(tf.float32, [None, num_classes])
-KEEP_PROB = tf.placeholder(tf.float32) # dropout (keep probability)
-REUSE = tf.placeholder(tf.bool)
-IS_TRAINING = tf.placeholder(tf.bool)
+Drop_rate = tf.placeholder(tf.float32) # dropout (keep probability)
+Is_training = tf.placeholder(tf.bool)
+
+def _extract_img(filename, numOfImages):
+    """Extract the images into a 4D tensor [image index, y, x, channels].
+    Values are rescaled from [0, 255] down to [-0.5, 0.5].
+    """
+    print('Extracting...', filename)
+    with gzip.open(filename, 'rb') as _bytestream:
+        _bytestream.read(16)
+        _buf = _bytestream.read(IMAGE_SIZE * IMAGE_SIZE * numOfImages)
+        _data = np.frombuffer(_buf, dtype=np.uint8).astype(np.float32)
+        _data = _data.reshape(numOfImages, IMAGE_SIZE * IMAGE_SIZE)
+    return _data
+
+
+def _extract_label(filename, numOfImages):
+    """Extract the labels into a vector of int64 label IDs."""
+    print('Extracting...', filename)
+    with gzip.open(filename, 'rb') as _bytestream:
+        _bytestream.read(8)
+        _buf = _bytestream.read(1 * numOfImages)
+        _labels = np.frombuffer(_buf, dtype=np.uint8).astype(np.int64)
+    return _labels
+
+def loadDataAndParse(imageFileName, labelFileName, numOfImages):
+    """Load images and lables according to names"""
+    _images = _extract_img(imageFileName, numOfImages)
+    _labels = _extract_label(labelFileName, numOfImages)
+    # convert to one_hot
+    _labels = np.eye(num_classes)[_labels]
+    return _images, _labels
 
 # Create the neural network
 def conv_net(x, n_classes, dropout, is_training):
@@ -64,53 +93,69 @@ def conv_net(x, n_classes, dropout, is_training):
 
     return out
 
+TRAIN_IMG_filename = "train-images-idx3-ubyte.gz"
+TRAIN_LBL_filename = "train-labels-idx1-ubyte.gz"
+TEST_IMG_filename = "t10k-images-idx3-ubyte.gz"
+TEST_LBL_filename = "t10k-labels-idx1-ubyte.gz"
+
+# load data
+train_img, train_labels = loadDataAndParse(TRAIN_IMG_filename, TRAIN_LBL_filename, 60000)
+test_img,  test_labels  = loadDataAndParse(TEST_IMG_filename, TEST_LBL_filename, 10000)
+# construct dataset
+train_set = tf.data.Dataset.from_tensor_slices((train_img, train_labels))\
+            .shuffle(buffer_size=10000).batch(128)
+test_set = tf.data.Dataset.from_tensor_slices((test_img, test_labels))\
+            .shuffle(buffer_size=1000).batch(256)
+
+it = tf.data.Iterator.from_structure(train_set.output_types, train_set.output_shapes)
+features, labels = it.get_next()
+
+training_init_op = it.make_initializer(train_set)
+test_init_op = it.make_initializer(test_set)
 
 # Construct model
-logits = conv_net(X, num_classes, KEEP_PROB, IS_TRAINING)
+logits = conv_net(features, num_classes, Drop_rate, Is_training)
 prediction = tf.nn.softmax(logits)
 
 # Define loss and optimizer
 loss_op = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(
-    logits=logits, labels=Y))
+    logits=logits, labels=labels))
 optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
 train_op = optimizer.minimize(loss_op)
 
 
 # Evaluate model
-correct_pred = tf.equal(tf.argmax(prediction, 1), tf.argmax(Y, 1))
+correct_pred = tf.equal(tf.argmax(prediction, 1), tf.argmax(labels,1))
 accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
 
 # Initialize the variables (i.e. assign their default value)
-init = tf.global_variables_initializer()
+init_val = tf.global_variables_initializer()
 
 # Start training
 with tf.Session() as sess:
 
     # Run the initializer
-    sess.run(init)
-
+    sess.run([init_val, training_init_op])
+    step = 1
     # Run
-    for step in range(1, num_steps+1):
-        batch_x, batch_y = mnist.train.next_batch(batch_size)
-        # Run optimization op (backprop)
-        sess.run(train_op, feed_dict={X: batch_x, Y: batch_y, KEEP_PROB: 0.8, IS_TRAINING: True})
-        if step % display_step == 0 or step == 1:
-            # Calculate batch loss and accuracy
-            loss, acc = sess.run([loss_op, accuracy], feed_dict={X: batch_x,
-                                                                 Y: batch_y,
-                                                                 KEEP_PROB: 1.0,
-                                                                 IS_TRAINING: False})
-            print("Step " + str(step) + ", Minibatch Loss= " + \
-                  "{:.4f}".format(loss) + ", Training Accuracy= " + \
-                  "{:.3f}".format(acc))
+    while True:
+        try:
+            # Run optimization op (backprop)
+            _, loss, acc = sess.run([train_op, loss_op, accuracy], feed_dict={Drop_rate: 0.0, Is_training: True})
+            if step % display_step == 0 or step == 1:
+                print("Step " + str(step) + ", Minibatch Loss= " + \
+                      "{:.4f}".format(loss) + ", Training Accuracy= " + \
+                      "{:.3f}".format(acc))
+            step = step + 1
+        except tf.errors.OutOfRangeError as e:
+            break
+        
 
     print("Optimization Finished!")
     # for t in tf.global_variables():
     #     print(t)
 
+    sess.run(test_init_op)
     # Calculate accuracy for 256 MNIST test images
     print("Testing Accuracy:", \
-        sess.run(accuracy, feed_dict={X: mnist.test.images[:256],
-                                      Y: mnist.test.labels[:256],
-                                      KEEP_PROB: 1.0,
-                                      IS_TRAINING: False}))
+        sess.run(accuracy, feed_dict={Drop_rate: 0.0, Is_training: False}))
